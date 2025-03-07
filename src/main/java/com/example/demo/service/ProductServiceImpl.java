d62cc9b226253ddf80ddb50ca790fe6f8a461d59
Anil <devarianil415@gmail.com>
2025-03-07 12:31:19 -0500

    added one more line psi

--- src/main/java/com/example/demo/service/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -21,8 +21,10 @@ public class ProductServiceImpl implements ProductService{
 	
 	private static final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
 	int i = 0;
-	int i += 4;
-	int i += 2;
+	i += 4;
+	i += 2;
+	i += 8;
+	
 
 	
 	@Override
